eddcb317a5de79f5429c775adb50cbf79328d5dc
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-06 20:59:27 -0500

    adding initial theme with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+          <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">(app)ropriate jokes</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Home</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/about">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
+
+
     <router-view/>
+    <footer class="bg-light py-5">
+            <div class="container"><div class="small text-center text-muted">Copyright © 2020 - (app)ropriate Jokes</div></div>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="home">
+      <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-lowercase text-white font-weight-bold">(app)ropriate Jokes</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 font-weight-light mb-5">Click to hear the joke.</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" v-on:click="showJoke()" href="#about">Make Me Laugh</a>
+                      <div>
+                        <p class="text-white-75 font-weight-light mb-34">{{ joke.setup }} </p>
+                        <p class="text-white-75 font-weight-light mb-34">{{ joke.punchline }}</p>
+                      </div>
+
+                    </div>
+                </div>
+            </div>
+        </header>
         <h1>{{ message }}</h1>
         <p>{{ subtext }}</p>
 
-    <button v-on:click="showJoke()">Make me laugh</button>
     <h2>{{ joke.setup }} </h2>
     <h3>{{ joke.punchline }} </h3>
     
