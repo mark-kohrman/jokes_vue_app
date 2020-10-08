60270bbc63ee6ae913551d993aad24c5d1245fc6
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-07 19:16:03 -0500

    touch ups before deployment

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,7 @@
                         <hr class="divider my-4" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 font-weight-light mb-5">Created by Mark Kohrman in 2020, (app)ropriate jokes is a simple app that tells you jokes that are almost as funny as the name of the app. Go to the home page and click the button to start laughing!  Even if the jokes are lame, the motive is pure.</p>
+                        <p class="text-white-75 font-weight-light mb-5">Created by Mark Kohrman in 2020, '(app)ropriate jokes' is a simple app that tells you jokes that are almost as funny as the name of the app. Go to the home page and click the button to start laughing!  Even if some are lame dad jokes, the motive is pure.</p>
                     </div>
                 </div>
             </div>
