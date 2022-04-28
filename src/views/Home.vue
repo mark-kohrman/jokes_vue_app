13484e7fb1b6f397d00713fdc3fdca1a469facf7
Mark-Kohrman <mark.kohrman@gmail.com>
2022-04-27 22:21:30 -0500

    fix vue to work with new API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-10 align-self-end">
-                        <h1 class="text-lowercase text-white font-weight-bold">(app)ropriate Jokes</h1>
+                        <h1 class="text-lowercase text-white font-weight-bold">(app)ropriate jokes</h1>
                         <hr class="divider my-4" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
@@ -13,8 +13,7 @@
                     
                       <div class="container">
                         <br>
-                        <h3 class="text text-white font-weight-bold">{{ joke.setup }} </h3>
-                        <h5 class="text-white-75 font-weight-light mb-34">{{ joke.punchline }}</h5>
+                        <h3 class="text text-white font-weight-bold">{{ joke.joke }} </h3>
                       </div>
 
                     </div>
@@ -33,18 +32,15 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "(app)ropriate Jokes",
+      message: "(app)ropriate jokes",
       subtext: "Click the button below to read a joke!",
       joke: [],
-      punchline: [],
-      setup: [],
     };
   },
   created: function () {},
   methods: {
     showJoke: function () {
-      axios.get(`/api/jokes/3`).then((response) => {
-        console.log(response);
+      axios.get(`/api/jokes/1`).then((response) => {
         this.joke = response.data;
       });
     },
